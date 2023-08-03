e137ebf647ed97355076af6c68ca1cceb784a924
yoanarobeva <yoana_robeva@yahoo.com>
2023-08-03 13:23:22 +0300

    try

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,8 +1,9 @@
 const config = {
     production: {
-        BASE_URL: ''
+        BASE_URL: 'https://c4-nexus-task.vercel.app'
     },
     development: {
+        // BASE_URL: 'https://c4-nexus-task.vercel.app'
         BASE_URL: 'http://localhost:3000'
     }
 }
